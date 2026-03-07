187d72e94e0ab5a1687fda5322a5d7a8b342594f
MatthewShabet <matthew.shabet@gmail.com>
2026-03-06 21:02:21 -0500

    Create fss.py

---
+++ reconumap/fss.py
@@ -0,0 +1,318 @@
+import numpy as np
+import scipy.sparse
+import numba
+SMOOTH_K_TOLERANCE = 1e-5
+MIN_K_DIST_SCALE = 1e-3
+NPY_INFINITY = np.inf
+
+# smooth_knn_dist
+# compute_membership_strengths
+# fuzzy_simplicial_set
+
+@numba.njit(
+    locals={
+        "psum": numba.types.float32,
+        "lo": numba.types.float32,
+        "mid": numba.types.float32,
+        "hi": numba.types.float32,
+    },
+    fastmath=True,
+)  # benchmarking `parallel=True` shows it to *decrease* performance
+def smooth_knn_dist(distances, k, n_iter=64, local_connectivity=1.0, bandwidth=1.0):
+    """Compute a continuous version of the distance to the kth nearest
+    neighbor. That is, this is similar to knn-distance but allows continuous
+    k values rather than requiring an integral k. In essence we are simply
+    computing the distance such that the cardinality of fuzzy set we generate
+    is k.
+
+    Parameters
+    ----------
+    distances: array of shape (n_samples, n_neighbors)
+        Distances to nearest neighbors for each sample. Each row should be a
+        sorted list of distances to a given samples nearest neighbors.
+
+    k: float
+        The number of nearest neighbors to approximate for.
+
+    n_iter: int (optional, default 64)
+        We need to binary search for the correct distance value. This is the
+        max number of iterations to use in such a search.
+
+    local_connectivity: int (optional, default 1)
+        The local connectivity required -- i.e. the number of nearest
+        neighbors that should be assumed to be connected at a local level.
+        The higher this value the more connected the manifold becomes
+        locally. In practice this should be not more than the local intrinsic
+        dimension of the manifold.
+
+    bandwidth: float (optional, default 1)
+        The target bandwidth of the kernel, larger values will produce
+        larger return values.
+
+    Returns
+    -------
+    knn_dist: array of shape (n_samples,)
+        The distance to kth nearest neighbor, as suitably approximated.
+
+    nn_dist: array of shape (n_samples,)
+        The distance to the 1st nearest neighbor for each point.
+    """
+    target = np.log2(k) * bandwidth
+    rho = np.zeros(distances.shape[0], dtype=np.float32)
+    result = np.zeros(distances.shape[0], dtype=np.float32)
+
+    mean_distances = np.mean(distances)
+
+    for i in range(distances.shape[0]):
+        lo = 0.0
+        hi = NPY_INFINITY
+        mid = 1.0
+
+        # TODO: This is very inefficient, but will do for now. FIXME
+        ith_distances = distances[i]
+        non_zero_dists = ith_distances[ith_distances > 0.0]
+        if non_zero_dists.shape[0] >= local_connectivity:
+            index = int(np.floor(local_connectivity))
+            interpolation = local_connectivity - index
+            if index > 0:
+                rho[i] = non_zero_dists[index - 1]
+                if interpolation > SMOOTH_K_TOLERANCE:
+                    rho[i] += interpolation * (
+                        non_zero_dists[index] - non_zero_dists[index - 1]
+                    )
+            else:
+                rho[i] = interpolation * non_zero_dists[0]
+        elif non_zero_dists.shape[0] > 0:
+            rho[i] = np.max(non_zero_dists)
+
+        for n in range(n_iter):
+
+            psum = 0.0
+            for j in range(1, distances.shape[1]):
+                d = distances[i, j] - rho[i]
+                if d > 0:
+                    psum += np.exp(-(d / mid))
+                else:
+                    psum += 1.0
+
+            if np.fabs(psum - target) < SMOOTH_K_TOLERANCE:
+                break
+
+            if psum > target:
+                hi = mid
+                mid = (lo + hi) / 2.0
+            else:
+                lo = mid
+                if hi == NPY_INFINITY:
+                    mid *= 2
+                else:
+                    mid = (lo + hi) / 2.0
+
+        result[i] = mid
+
+        # TODO: This is very inefficient, but will do for now. FIXME
+        if rho[i] > 0.0:
+            mean_ith_distances = np.mean(ith_distances)
+            if result[i] < MIN_K_DIST_SCALE * mean_ith_distances:
+                result[i] = MIN_K_DIST_SCALE * mean_ith_distances
+        else:
+            if result[i] < MIN_K_DIST_SCALE * mean_distances:
+                result[i] = MIN_K_DIST_SCALE * mean_distances
+
+    return result, rho
+
+
+@numba.njit(
+    locals={
+        "knn_dists": numba.types.float32[:, ::1],
+        "sigmas": numba.types.float32[::1],
+        "rhos": numba.types.float32[::1],
+        "val": numba.types.float32,
+    },
+    parallel=True,
+    fastmath=True,
+)
+def compute_membership_strengths(
+    knn_indices,
+    knn_dists,
+    sigmas,
+    rhos,
+    return_dists=False,
+    bipartite=False,
+):
+    """Construct the membership strength data for the 1-skeleton of each local
+    fuzzy simplicial set -- this is formed as a sparse matrix where each row is
+    a local fuzzy simplicial set, with a membership strength for the
+    1-simplex to each other data point.
+
+    Parameters
+    ----------
+    knn_indices: array of shape (n_samples, n_neighbors)
+        The indices on the ``n_neighbors`` closest points in the dataset.
+
+    knn_dists: array of shape (n_samples, n_neighbors)
+        The distances to the ``n_neighbors`` closest points in the dataset.
+
+    sigmas: array of shape(n_samples)
+        The normalization factor derived from the metric tensor approximation.
+
+    rhos: array of shape(n_samples)
+        The local connectivity adjustment.
+
+    return_dists: bool (optional, default False)
+        Whether to return the pairwise distance associated with each edge.
+
+    bipartite: bool (optional, default False)
+        Does the nearest neighbour set represent a bipartite graph? That is, are the
+        nearest neighbour indices from the same point set as the row indices?
+
+    Returns
+    -------
+    rows: array of shape (n_samples * n_neighbors)
+        Row data for the resulting sparse matrix (coo format)
+
+    cols: array of shape (n_samples * n_neighbors)
+        Column data for the resulting sparse matrix (coo format)
+
+    vals: array of shape (n_samples * n_neighbors)
+        Entries for the resulting sparse matrix (coo format)
+
+    dists: array of shape (n_samples * n_neighbors)
+        Distance associated with each entry in the resulting sparse matrix
+    """
+    n_samples = knn_indices.shape[0]
+    n_neighbors = knn_indices.shape[1]
+
+    rows = np.zeros(knn_indices.size, dtype=np.int32)
+    cols = np.zeros(knn_indices.size, dtype=np.int32)
+    vals = np.zeros(knn_indices.size, dtype=np.float32)
+    if return_dists:
+        dists = np.zeros(knn_indices.size, dtype=np.float32)
+    else:
+        dists = None
+
+    for i in range(n_samples):
+        for j in range(n_neighbors):
+            if knn_indices[i, j] == -1:
+                continue  # We didn't get the full knn for i
+            # If applied to an adjacency matrix points shouldn't be similar to themselves.
+            # If applied to an incidence matrix (or bipartite) then the row and column indices are different.
+            if (bipartite == False) & (knn_indices[i, j] == i):
+                val = 0.0
+            elif knn_dists[i, j] - rhos[i] <= 0.0 or sigmas[i] == 0.0:
+                val = 1.0
+            else:
+                val = np.exp(-((knn_dists[i, j] - rhos[i]) / (sigmas[i])))
+
+            rows[i * n_neighbors + j] = i
+            cols[i * n_neighbors + j] = knn_indices[i, j]
+            vals[i * n_neighbors + j] = val
+            if return_dists:
+                dists[i * n_neighbors + j] = knn_dists[i, j]
+
+    return rows, cols, vals, dists
+
+
+def fuzzy_simplicial_set(
+    X,
+    n_neighbors,
+    set_op_mix_ratio=1.0,
+    local_connectivity=1.0,
+    apply_set_operations=True,
+):
+    """Given a set of data X, a neighborhood size, and a measure of distance
+    compute the fuzzy simplicial set (here represented as a fuzzy graph in
+    the form of a sparse matrix) associated to the data. This is done by
+    locally approximating geodesic distance at each point, creating a fuzzy
+    simplicial set for each such point, and then combining all the local
+    fuzzy simplicial sets into a global one via a fuzzy union.
+
+    Parameters
+    ----------
+    X: array of shape (n_samples, n_features)
+        The data to be modelled as a fuzzy simplicial set.
+
+    n_neighbors: int
+        The number of neighbors to use to approximate geodesic distance.
+        Larger numbers induce more global estimates of the manifold that can
+        miss finer detail, while smaller values will focus on fine manifold
+        structure to the detriment of the larger picture.
+
+    knn_indices: array of shape (n_samples, n_neighbors) (optional)
+        If the k-nearest neighbors of each point has already been calculated
+        you can pass them in here to save computation time. This should be
+        an array with the indices of the k-nearest neighbors as a row for
+        each data point.
+
+    knn_dists: array of shape (n_samples, n_neighbors) (optional)
+        If the k-nearest neighbors of each point has already been calculated
+        you can pass them in here to save computation time. This should be
+        an array with the distances of the k-nearest neighbors as a row for
+        each data point.
+
+    set_op_mix_ratio: float (optional, default 1.0)
+        Interpolate between (fuzzy) union and intersection as the set operation
+        used to combine local fuzzy simplicial sets to obtain a global fuzzy
+        simplicial sets. Both fuzzy set operations use the product t-norm.
+        The value of this parameter should be between 0.0 and 1.0; a value of
+        1.0 will use a pure fuzzy union, while 0.0 will use a pure fuzzy
+        intersection.
+
+    local_connectivity: int (optional, default 1)
+        The local connectivity required -- i.e. the number of nearest
+        neighbors that should be assumed to be connected at a local level.
+        The higher this value the more connected the manifold becomes
+        locally. In practice this should be not more than the local intrinsic
+        dimension of the manifold.
+
+    Returns
+    -------
+    fuzzy_simplicial_set: coo_matrix
+        A fuzzy simplicial set represented as a sparse matrix. The (i,
+        j) entry of the matrix represents the membership strength of the
+        1-simplex between the ith and jth sample points.
+    """
+
+    assert set_op_mix_ratio >= 0.0 and set_op_mix_ratio <= 1.0
+    
+    knn_indices = np.zeros((X.shape[0], n_neighbors), dtype=int)
+    knn_dists = np.zeros(knn_indices.shape, dtype=float)
+    # Find KNNs row-by-row
+    for row_id in range(X.shape[0]):
+        row_data = X[row_id].data
+        row_indices = X[row_id].indices
+        assert len(row_data) >= n_neighbors
+        row_nn_data_indices = np.argsort(row_data)[:n_neighbors]
+        knn_indices[row_id] = row_indices[row_nn_data_indices]
+        knn_dists[row_id] = row_data[row_nn_data_indices]
+    knn_dists = knn_dists.astype(np.float32)
+    assert (knn_dists <= 2).all()
+
+    sigmas, rhos = smooth_knn_dist(
+        knn_dists,
+        float(n_neighbors),
+        local_connectivity=float(local_connectivity),
+    )
+
+    rows, cols, vals, dists = compute_membership_strengths(
+        knn_indices, knn_dists, sigmas, rhos, False
+    )
+
+    result = scipy.sparse.coo_matrix(
+        (vals, (rows, cols)), shape=(X.shape[0], X.shape[0])
+    )
+    result.eliminate_zeros()
+
+    if apply_set_operations:
+        transpose = result.transpose()
+
+        prod_matrix = result.multiply(transpose)
+
+        result = (
+            set_op_mix_ratio * (result + transpose - prod_matrix)
+            + (1.0 - set_op_mix_ratio) * prod_matrix
+        )
+
+    result.eliminate_zeros()
+
+    return result
